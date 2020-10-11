19a3bb71eff0614c53c1fe26c6d43357366b5c7b
Timothy Nguyen <quynhthoa1972@gmail.com>
2020-10-11 09:46:03 -0400

    Updates

---
+++ precache-manifest.a6c557aaec21bca5f8b2d89565d4e9b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31108763198ff0a3ae82a0746ae6a82e",
+    "url": "/TimothyNguyen/index.html"
+  },
+  {
+    "revision": "ae5a023070c8cb28bc44",
+    "url": "/TimothyNguyen/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "8314abe871836fba1bca",
+    "url": "/TimothyNguyen/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ae5a023070c8cb28bc44",
+    "url": "/TimothyNguyen/static/js/2.50e856fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TimothyNguyen/static/js/2.50e856fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8314abe871836fba1bca",
+    "url": "/TimothyNguyen/static/js/main.3bd379a9.chunk.js"
+  },
+  {
+    "revision": "58a092609422db840a51",
+    "url": "/TimothyNguyen/static/js/runtime-main.db78d55e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/TimothyNguyen/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- src/pages/ContactPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ContactPage(props){
-    return(
-        <p>Contact Page</p>
-    );
-}
-
-export default ContactPage;
