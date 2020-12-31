9e340670307cd4549a4138ef8b851ce3ddb4adb4
Timothy Nguyen <quynhthoa1972@gmail.com>
2020-12-31 02:17:25 -0500

    Create light/dark theme

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -4,14 +4,29 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import HomePage from './pages/HomePage'
 import Resume from './pages/Resume';
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./components/globalStyles";
+import { lightTheme, darkTheme } from "./components/Themes"
+import  {useDarkMode} from "./components/useDarkMode"
+import Toggle from "./components/Toggle"
 function App() {
+
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+  if(!mountedComponent) return <div/>
     return (
-    <BrowserRouter>
-       <Switch>
-        <Route exact path="/TimothyNguyen" component={HomePage} />
-        <Route path="/TimothyNguyen/SWEResume.pdf" component={Resume} />
-      </Switch>
-    </BrowserRouter>
+      <ThemeProvider theme={themeMode}>
+      <>
+      <GlobalStyles/>
+        <Toggle theme={theme} toggleTheme={themeToggler} />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/TimothyNguyen" component={HomePage} />
+            <Route path="/TimothyNguyen/SWEResume.pdf" component={Resume} />
+          </Switch>
+        </BrowserRouter>
+    </>
+    </ThemeProvider>
     );
 }   
 
